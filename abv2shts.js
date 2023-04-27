d7298f0ebc62ec596d866e4084640160c59b9e51
Patrick Bryant <patrick.bryant@emory.edu>
2023-04-27 15:10:13 -0400

    Refactored to separate the math and to improve the innerHTML

--- abv2shts.js
+++ abv2shts.js
@@ -1,3 +1,29 @@
+function dotheMath(inVolume = 0, abv_arg = 0, inUnits='oz'){
+  if (inVolume <= 0 || abv_arg <= 0) { 
+      console.error("Volume and ABV must have positive values.");
+      return; 
+  }
+  if (!['ml','oz'].includes(inUnits)){
+    console.error(`Units must be ml or oz; you sent ${inUnits}.`);
+    return;
+  }
+  const abv = abv_arg / 100;
+  const ozToMl = 29.57;
+  const mlShot = 20;
+  const ozShot = .6;
+  const mlVolume = inUnits === "ml" ? inVolume : inVolume * ozToMl;
+  const ozVolume = inUnits === "oz" ? inVolume : inVolume / ozToMl;
+  return {
+        mlVolume : mlVolume.toLocaleString('en-US', {maximumFractionDigits: 2}),
+        ozVolume : ozVolume.toLocaleString('en-US', {maximumFractionDigits: 2}),
+        mlAlcohol : (abv * mlVolume).toLocaleString('en-US', {maximumFractionDigits: 2}),
+        ozAlcohol : (abv * ozVolume).toLocaleString('en-US', {maximumFractionDigits: 2}),
+        ozShots : ((abv * mlVolume) / ozShot).toLocaleString('en-US', {maximumFractionDigits: 2}),
+        mlShots : ((abv * ozVolume) / mlShot).toLocaleString('en-US', {maximumFractionDigits: 2})
+  }
+  
+}
+
 function calculateShots()
 {
   let inUnits = document.getElementById("inUnits").value;
@@ -7,21 +33,15 @@ function calculateShots()
   	{ 
   		return; 
 	}
-  let ozToMl = 29.57;
-  let mlShot = 20;
-  let ozShot = .6;
-  let mlVolume = inUnits === "ml" ? inVolume : inVolume * ozToMl;
-  let ozVolume = inUnits === "oz" ? inVolume : inVolume / ozToMl;
-  let mlAlcohol = abv * mlVolume;
-  let ozAlcohol = abv * ozVolume;
-  let ozShots = ozAlcohol / ozShot;
-  let mlShots = mlAlcohol / mlShot;
-  
-  let outputDiv = document.getElementById("output");
+  const data = dotheMath(inVolume, abv, inUnits);
+  const outputDiv = document.getElementById("output");
   outputDiv.innerHTML = 
-    `Volume: ${Math.round(mlVolume * 100)/100} ml; ${Math.round(ozVolume *100)/100} oz<br />` +
-    `Alcohol: ${Math.round(mlAlcohol * 100)/100} ml; ${Math.round(ozAlcohol * 100)/100} oz<br>`+
-    `1.5 oz shots: ${Math.round(ozShots * 100)/100} <br>`+
-    `50 ml shots: ${Math.round(mlShots * 100)/100}`;
+`
+
+<div class="total_volume">Input Volume: ${data.mlVolume} ml; ${data.ozVolume} oz</div>
+<div class="alc_volume">Alcohol: ${data.mlAlcohol} ml; ${data.ozAlcohol} oz</div>
+<div class="shots"><span class="shots_label">1.5 oz shots</span><span class="shots_value"> ${data.ozShots} </span></div>
+<div class="shots"><span class="shots_label">50 ml shots<span><span class="shots_val"> ${data.mlShots}</span></div>
+`;
 
 }
